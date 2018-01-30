1a14e8325c72a8dc34c5a4e0af85a967732cb9cb
Bear Golightly <bear@nashvillewraps.com>
2018-01-30 14:33:35 -0600

    cleaned up

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,38 +1,28 @@
-
-set-psdebug -trace 1
-Set-PSReadlineOption -BellStyle None
-set-psdebug -trace 0
-
 $script_library = "C:\ps_profile_scripts\" # path ending with \
-
-# $env:path += ";$env:userprofile\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\cmd"
-# $env:path += ";$env:userprofile\AppData\Local\GitHub\PortableGit_d76a6a98c9315931ec4927243517bc09e9b731a0\usr\bin"
 $env:path += ";C:\Program Files (x86)\Microsoft VS Code"
-
-$MaximumHistoryCount = 1000
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-Get-ChildItem ($script_library + "*.ps1") | ForEach-Object { . (Join-Path $script_library $_.Name)}
-
 $modules = "posh-git","psake","pester"
+
+# disable terminal bells and beeps
+Set-PSReadlineOption -BellStyle None -MaximumHistoryCount 1000
+
+
+# load named modules, if they exist, and show a version table
 foreach ($module in $modules) {
-  if (Get-Module $module) {
-    Write-Host "Loading $($module) v$($module.version)"
-    Import-Module $module -Verbose
+  if (Get-Module -listavailable -name $module) {
+    Import-Module $module
+    # Write-Host "Loaded $($module) v$((get-module $module).version)"
   }  
-
 }
+Get-Module $modules | Format-Table name, version
 
-#Import-Module psake
-#Import-Module posh-git
-
+Get-ChildItem ($script_library + "*.ps1") | 
+  ForEach-Object { . (Join-Path $script_library $_.Name)}
 
 function changelog {
   $t = git describe --tags --abbrev=0
   git log "$t...HEAD" --oneline
 }
-    
+
+function newwin {
+  Invoke-Item C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+}
--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,5 @@
 task default -depends Copy 
 
 task Copy {
-    Copy-Item -Force Microsoft.PowerShell_profile.ps1 $PROFILE
+    Copy-Item -Force Microsoft.PowerShell_profile.ps1 $PROFILE -Verbose
 }
